bff877a7fd92ceb3babd8be068c90f8faf7d3c49
Shubhendu Shukla <drs79sjiw@gmail.com>
2025-05-21 20:32:00 +0530

    mm

--- app.js
+++ app.js
@@ -7,7 +7,13 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 // Middleware
-app.use(cors());
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+app.use(cors(corsOptions))
 app.use(express.json());
 
 // Nodemailer Transporter Setup
